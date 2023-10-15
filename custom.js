46edbea9ed7eddd5cc4baa56a68a52a1c528f51f
Ann Tech <anntech21@gmail.com>
2023-10-14 17:40:17 -0700

    new attempt

---
+++ custom.js
@@ -0,0 +1,12 @@
+console.log ("Hello, World!");
+console.log("Anchal");
+console.log("2" + "2"); //string
+console.log(2+2); //number
+
+//alert (!)
+alert ("Hello, World!");
+alert ("Accept Cookies");
+
+//prompts
+let firstName = prompt ("What is your first name?");
+console.log (firstName);
